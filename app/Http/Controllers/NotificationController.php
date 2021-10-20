f6d95ecde8aa75c29c622dd7e206ba3c4f627894
mhamzase <mhamzasulehri143@gmail.com>
2021-10-20 15:42:30 +0500

    initial commit

---
+++ app/Http/Controllers/NotificationController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Notification;
+use App\Notifications\OffersNotification;
+
+class NotificationController extends Controller
+{
+    public function sendOfferNotification() {
+        $userSchema = User::first();
+  
+        $offerData = [
+            'name' => 'BOGO',
+            'body' => 'You received an offer.',
+            'thanks' => 'Thank you',
+            'offerText' => 'Check out the offer',
+            'offerUrl' => url('/'),
+            'offer_id' => 007
+        ];
+  
+        Notification::send($userSchema, new OffersNotification($offerData));
+   
+        dd('Task completed!');
+    }
+}
---
+++ routes/web.php
@@ -0,0 +1,9 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\NotificationController;
+
+
+Route::get('/send-notification', [NotificationController::class, 'sendOfferNotification']);
+
+Route::view('/','welcome');
